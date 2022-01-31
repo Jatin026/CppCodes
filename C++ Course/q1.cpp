428aa8727145038233277ff4a0c3a7f55772767c
Jatin <26.jatinhanda@gmail.com>
2022-01-31 22:42:09 +0530

    commit

---
+++ C++ Course/q1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t1,t2,t3,t4,t5;
+    long long int k1,k2,k3;
+    string l1,l2,l3;
+    int* ptr=&t1;
+
+    cout<<ptr<<"\t"<<&t2<<"\t"<<&t3<<"\t"<<&t4<<"\t"<<&t5<<endl;
+    cout<<&k1<<"\t"<<&k2<<"\t"<<&k3<<endl;
+    cout<<&l1<<"\t"<<&l2<<"\t"<<&l3<<endl;
+    cout<<sizeof(double);
+     
+    return 0;
+}
