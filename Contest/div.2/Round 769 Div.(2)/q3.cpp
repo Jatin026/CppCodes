ac04e3b6a7194a5d0175f1fc3f161d0510c0d90c
Jatin <26.jatinhanda@gmail.com>
2022-02-04 20:29:45 +0530

    update

---
+++ Contest/div.2/Round 769 Div.(2)/q3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main(){
+    FAST
+    int t;
+ 
+    }
+    return 0;
+}
